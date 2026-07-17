35e4e958ec3a95809f6730255038e349fabb5c0a
Jorgegh7 <luzzdeurano@gmail.com>
2026-07-17 14:16:53 -0400

    Flujo Carrito V1.1

--- src/main/java/com/minimarket/service/impl/CarritoServiceImpl.java
+++ src/main/java/com/minimarket/service/impl/CarritoServiceImpl.java
@@ -3,7 +3,11 @@ package com.minimarket.service.impl;
 import com.minimarket.dto.carrito.CarritoRequestDTO;
 import com.minimarket.dto.carrito.CarritoResponseDTO;
 import com.minimarket.entity.Carrito;
+import com.minimarket.entity.Producto;
+import com.minimarket.entity.Usuario;
 import com.minimarket.repository.CarritoRepository;
+import com.minimarket.repository.ProductoRepository;
+import com.minimarket.repository.UsuarioRepository;
 import com.minimarket.service.CarritoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +18,13 @@ import java.util.List;
 public class CarritoServiceImpl implements CarritoService {
 
     private final CarritoRepository carritoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final ProductoRepository productoRepository;
 
-    public CarritoServiceImpl(CarritoRepository carritoRepository) {
+    public CarritoServiceImpl(CarritoRepository carritoRepository, UsuarioRepository usuarioRepository, ProductoRepository productoRepository) {
         this.carritoRepository = carritoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.productoRepository = productoRepository;
     }
 
     @Override
@@ -46,7 +54,21 @@ public class CarritoServiceImpl implements CarritoService {
 
     @Override
     public CarritoResponseDTO agregarProducto(CarritoRequestDTO request) {
-        return null;
+        Carrito carrito = new Carrito();
+        Usuario usuario = usuarioRepository.findById(request.usuarioId()).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        Producto producto = productoRepository.findById(request.productoId()).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        carrito.setUsuario(usuario);
+        carrito.setProducto(producto);
+        carrito.setCantidad(request.cantidad());
+
+        Carrito creado = carritoRepository.save(carrito);
+        return new CarritoResponseDTO(
+                creado.getId(),
+                creado.getUsuario().getId(),
+                creado.getUsuario().getUsername(),
+                creado.getProducto().getId(),
+                creado.getProducto().getNombre(),
+                creado.getCantidad());
     }
 
     @Override
