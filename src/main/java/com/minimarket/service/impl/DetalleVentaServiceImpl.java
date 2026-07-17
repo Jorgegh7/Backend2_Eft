d299fd599ca5c504938787384a71f19af8863ff2
Jorgegh7 <luzzdeurano@gmail.com>
2026-07-17 14:55:09 -0400

    Flujo DetalleVenta V1.2

--- src/main/java/com/minimarket/service/impl/DetalleVentaServiceImpl.java
+++ src/main/java/com/minimarket/service/impl/DetalleVentaServiceImpl.java
@@ -3,7 +3,11 @@ package com.minimarket.service.impl;
 import com.minimarket.dto.detalleVenta.DetalleVentaRequestDTO;
 import com.minimarket.dto.detalleVenta.DetalleVentaResponseDTO;
 import com.minimarket.entity.DetalleVenta;
+import com.minimarket.entity.Producto;
+import com.minimarket.entity.Venta;
 import com.minimarket.repository.DetalleVentaRepository;
+import com.minimarket.repository.ProductoRepository;
+import com.minimarket.repository.VentaRepository;
 import com.minimarket.service.DetalleVentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +17,17 @@ import java.util.List;
 @Service
 public class DetalleVentaServiceImpl implements DetalleVentaService {
 
-    @Autowired
-    private DetalleVentaRepository detalleVentaRepository;
+    private final DetalleVentaRepository detalleVentaRepository;
+    private final VentaRepository ventaRepository;
+    private final ProductoRepository productoRepository;
+
+    public DetalleVentaServiceImpl(DetalleVentaRepository detalleVentaRepository, VentaRepository ventaRepository, ProductoRepository productoRepository) {
+        this.detalleVentaRepository = detalleVentaRepository;
+        this.ventaRepository = ventaRepository;
+        this.productoRepository = productoRepository;
+    }
+
+
 
 
     @Override
@@ -58,6 +71,21 @@ public class DetalleVentaServiceImpl implements DetalleVentaService {
 
     @Override
     public DetalleVentaResponseDTO agregarDetalle(DetalleVentaRequestDTO request) {
-        return null;
+        Venta venta = ventaRepository.findById(request.ventaId()).orElseThrow(() -> new RuntimeException("Venta no encontrada"));
+        Producto producto = productoRepository.findById(request.productoId()).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+
+        DetalleVenta detalleVenta = new DetalleVenta();
+        detalleVenta.setVenta(venta);
+        detalleVenta.setProducto(producto);
+        detalleVenta.setCantidad(request.cantidad());
+        detalleVenta.setPrecio(producto.getPrecio());
+
+        DetalleVenta detalleCreado = detalleVentaRepository.save(detalleVenta);
+        return new DetalleVentaResponseDTO(
+                detalleCreado.getId(),
+                detalleCreado.getProducto().getId(),
+                detalleCreado.getProducto().getNombre(),
+                detalleCreado.getCantidad(),
+                detalleCreado.getPrecio());
     }
 }
